9ef53a2ef95bc55381797761e5c193a46e08a25b
ksheyon123 <kshyeon123@gmail.com>
2020-01-02 17:35:10 +0900

    20200102)1734-db setup

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ app.use((req, res, next) => {
 var server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
-// console.log(app)
+
 webSocket(server);
--- socket.js
+++ socket.js
@@ -1,31 +1,5 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io').listen(server);
 
-module.exports = (server) => {
-    const wss = new WebSocket.Server({server});
-    console.log(wss)
-    wss.on('connection', (ws, req) => {
-        console.log('hi')
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
 
-        ws.on('message', (message) => {
-            console.log('WebSocket Message', message);
-        });
 
-        ws.on('error', (err) => {
-            console.log('WebSocket Err', error);
-        });
-
-        ws.on('close', () => {
-            console.log('클라이언트 접속 해제');
-            clearInterval(ws.interval);
-        });
-
-        const interval = setInterval(() => {
-            if (ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메세지를 보냅니다.');
-            }
-        }, 3000);
-        ws.interval = interval;
-    });
-};
+module.exports = socket;
