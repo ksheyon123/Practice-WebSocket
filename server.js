96298b0fd7cdee9386f2f83311c301deda23766e
ksheyon123 <kshyeon123@gmail.com>
2020-01-03 11:21:26 +0900

    20200103)1121-Sending Message 1:1 ongoing

--- server.js
+++ server.js
@@ -43,19 +43,41 @@ io.sockets.on('connection', function(socket) {
         if (message.recepient == 'ALL') {
             console.dir('나를 포함한 모든 클라이언트에게 message 이벤트를 전송합니다.');
             io.sockets.emit('message', message);
+        } else {
+            console.log('message recepient', login_ids)
+            if (login_ids[message.recepient]) {
+                io.sockets.connected[login_ids[message.recepient]].emit('message', message);
+
+                sendResponse(socket, 'message', '200', '메세지를 전송했습니다.');
+            } else {
+                sendResponse(socket, 'login', '404', '상대방의 로그인 ID를 찾을 수 없습니다.');
+            }
         }
     });
 
-    var login_ids = {};
-
+    var login_ids= {};
     socket.on('login', (login) => {
         console.log('login 이벤트를 받았습니다.');
         console.dir(login);
 
         console.log('접속한 소켓의 ID : ', socket.id);
+        // var data = makeObject(login.id, socket.id);
+        // login_ids.push(data);
         login_ids[login.id] = socket.id;
         socket.login_id = login.id;
+        console.log('접속한 클라이언트 ID 개수 :  %d', Object.keys(login_ids).length);
 
-        console.log('접속한 클라이언트 ID 개수 :  %d', Object.keys(login_ids).legnth);
-    })
+        sendResponse(socket, 'login', '200', '로그인되었습니다.');
+    });
+
+    function sendResponse(socket, command, code, message) {
+        var statusObj = {command: command, code: code, message: message};
+        socket.emit('response', statusObj);
+    }
+
+    function makeObject(attr, value) {
+        var data = new Object();
+        data[attr] = value;
+        return data;
+    }
 });
